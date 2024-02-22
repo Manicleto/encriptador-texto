dd6a89c1657a5f77b46a727e35030534ccfe9c68
Manuel Ruiz F <mruiz.fig@gmail.com>
2024-02-21 23:12:35 -0700

    Adicion de la funcion para desencriptar un texto

--- index.js
+++ index.js
@@ -5,21 +5,49 @@ function encriptar(){
     let muñeco = document.getElementById("muñeco");
     
     let textoCifrado=texto
-        .replace(/i/gi, "imes")
         .replace(/e/gi, "enter")
+        .replace(/i/gi, "imes")
         .replace(/a/gi, "ai")
         .replace(/o/gi, "ober")
         .replace(/u/gi, "ufat");
 
-        if (texto.length!=0) {
-            texto = document.getElementById("texto").value=textoCifrado;
+
+        if (texto.length != 0) {
+            document.getElementById("texto").value=textoCifrado;
             tituloMensaje.textContent= "Texto encriptado con exito";
             parrafo.textContent = "";
-            muñeco="./img/encriptado.jpg";
+            muñeco.src = "./img/encriptado.jpg";
         }else{
-            muñeco="./img/meñeco.jpg";
+            muñeco.src = "./img/meñeco.jpg";
             tituloMensaje.textContent="Ningun mensaje fue encontrado"
             parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar"
             alert("Debes ingresar algun texto");
         }
+}
+
+function desencriptar(){
+    let texto = document.getElementById("texto").value;
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo");
+    let muñeco = document.getElementById("muñeco");
+    
+    let textoCifrado = texto
+        .replace(/enter/gi, "e")
+        .replace(/imes/gi, "i")
+        .replace(/ai/gi, "a")
+        .replace(/ober/gi, "o")
+        .replace(/ufat/gi, "u");
+
+        if (texto.length!=0) {
+            texto = document.getElementById("texto").value=textoCifrado;
+            tituloMensaje.textContent= "Texto desencriptado con exito";
+            parrafo.textContent = "";
+            muñeco.src="./img/desencriptado.jpg";
+        }else{
+            muñeco.src="./img/meñeco.jpg";
+            tituloMensaje.textContent="Ningun mensaje fue encontrado"
+            parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar"
+            alert("Debes ingresar algun texto");
+        }
+
 }
